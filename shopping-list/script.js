93f350823acf6fb64abd3c4e31d4cdc902a604ac
poncardasm <159589562+poncardasm@users.noreply.github.com>
2024-07-06 03:59:27 +0300

    feat: add item to DOM

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -1,3 +1,43 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+
+function addItem(e) {
+  e.preventDefault();
+  
+  const newItem = itemInput.value;
+  
+  // Validate input
+  if (newItem === '') {
+    alert('Please add an item');
+    return;
+  }
+
+  // Create list item
+  const addToList = document.createElement('li');
+  addToList.appendChild(document.createTextNode(newItem));
+
+  const button = createButton('remove-item btn-link text-red')
+  addToList.appendChild(button);
+  itemList.appendChild(addToList);
+
+  itemInput.value = '';
+}
+
+function createButton(btnClass) {
+  const button = document.createElement('button');
+  button.className = btnClass;
+
+  const icon = createIcon('fa-solid fa-xmark');
+  button.appendChild(icon);
+  return button;
+}
+
+function createIcon(iconClass) {
+  const icon = document.createElement('i');
+  icon.className = iconClass;
+  return icon;
+}
+
+// Event listeners
+itemForm.addEventListener('submit', addItem);
